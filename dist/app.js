ed6b7d94a4bd18a0b8a02e3fa59c94bef3dc007f
Saleh-Alibrahim <s.albrahim@outlook.com>
2022-12-06 12:09:12 +0300

    compile

--- dist/app.js
+++ dist/app.js
@@ -9,6 +9,7 @@ const auth_route_1 = __importDefault(require("./routes/auth.route"));
 const todo_route_1 = __importDefault(require("./routes/todo.route"));
 require("dotenv/config");
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.static('./client/build'));
@@ -17,7 +18,7 @@ app.use((0, cors_1.default)());
 app.use('/api/v1/auth', auth_route_1.default);
 app.use('/api/v1/todo', todo_route_1.default);
 app.use((req, res, next) => {
-    return res.sendFile('./client/build/index.html');
+    return res.sendFile(path_1.default.join(__dirname, '../client/build/index.html'));
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
